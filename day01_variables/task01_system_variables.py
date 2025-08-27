c5f964b11f02e69c8fbf3845e18f02383f5d81b5
Rahul Kohli <rahulkohli3192@gmail.com>
2025-08-27 18:15:33 +0530

    Initial commit: 30-day Python DevOps learning structure

---
+++ day01_variables/task01_system_variables.py
@@ -0,0 +1,11 @@
+hostname = "rahul-os"
+ram_gb = 24.5
+cpu_cores = 8
+disk_gb = 512
+is_production = True
+
+print(f"Server: {hostname}")
+print(f"RAM: {ram_gb}GB")
+print(f"CPU: {cpu_cores}CORES")
+print(f"Disk Space: {disk_gb}GB")
+print(f"Production: {is_production}")
